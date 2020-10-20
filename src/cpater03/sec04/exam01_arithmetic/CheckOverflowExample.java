82917e85f7fd5f50e740e300a7748c2f97cdab01
hotan486 <hotam486@naver.com>
2020-10-20 22:11:57 +0900

    ㄴㅇㄹ

--- src/cpater03/sec04/exam01_arithmetic/CheckOverflowExample.java
+++ src/cpater03/sec04/exam01_arithmetic/CheckOverflowExample.java
@@ -19,13 +19,18 @@ public class CheckOverflowExample {
 		System.out.println("left:" +(Integer.MAX_VALUE - right));
 		System.out.println("left:" +(Integer.MIN_VALUE-right));
 		
-		
+		//6 + 6
+		//max 10 
+		//10 - 6 = 4
+		// 6 6
+		//-10 -6  10 9 8 7 6 5 > 6
 		if((right>0)) { 
-			if(left>(Integer.MAX_VALUE - right)) {
+			if(left>(Integer.MAX_VALUE - right)) {  //맥스값에서 right를 뺴면 right에 더할수있는최대치가 나오는데 그 값보다 크면 오류
+				
 				throw new ArithmeticException("오버플로우 발생");
 			}
 		} else {
-			if(left<(Integer.MIN_VALUE - right)) {
+			if(left<(Integer.MIN_VALUE - right)) { // 
 				throw new ArithmeticException("오버플로우 발생");
 			}
 		}
